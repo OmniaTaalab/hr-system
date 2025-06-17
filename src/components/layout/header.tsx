744df063ab8e5090b6299d95e1cf25bd2c2ab868
Abdelghani Mohamed Yasser ElSabagh <abdelghani.elsabagh@nis-egypt.com>
2025-06-17 10:28:19 +0000

    لماذا لاتوجد صورة او ايقونة على القائمة المنسدلة

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -16,7 +16,7 @@ import Link from "next/link";
 export function Header() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between_ sm:justify-end_ px-4 sm:px-8">
+      <div className="container flex h-16 items-center justify-between sm:justify-end px-4 sm:px-8">
         <div className="md:hidden">
           <SidebarTrigger />
         </div>
