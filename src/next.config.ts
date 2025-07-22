27ee486914444a724e76d68ba9e6ca308a3ef581
Abdelghani Mohamed Yasser ElSabagh <abdelghani.elsabagh@nis-egypt.com>
2025-07-22 12:21:31 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/next.config.ts
+++ src/next.config.ts
@@ -4,9 +4,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
- allowedDevOrigins: [
-      '9000-firebase-studio-1750152482411.cluster-6frnii43o5blcu522sivebzpii.cloudworkstations.dev',
- ],
     ignoreBuildErrors: true,
   },
   eslint: {
