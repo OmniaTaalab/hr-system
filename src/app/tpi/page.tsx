fb4f434201e7472a0c92c150b01ff13dbf793e5f
Omnia Zayed Taalab Abo ElHassan <omnia.taalab@nis-egypt.com>
2025-08-04 13:32:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/tpi/page.tsx
+++ src/app/tpi/page.tsx
@@ -93,7 +93,7 @@ function TpiManagementContent() {
                 const apiToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im9tbmlhIHRhYWxhYiIsImlkIjoyMjI3MTAsInJvbGUiOiJzdXBlciBhZG1pbiIsImRvbWFpbiI6bnVsbCwiaWF0IjoxNzU0NjQ4MzgzLCJleHAiOjE3NTQ3MzQ3ODN9.hX0o2w-JbCo_q3Qx36dCi5tOWV925sLg6gTupSMDkI8";
                 const response = await fetch(`https://blb-staging-hwnidclrba-uc.a.run.app/reports/leaderBoard?stage_tag_ids=${selectedStageTagId}`, {
                     headers: {
-                        'Authorization': `Bearer ${apiToken}`
+                        'Authorization': ` ${apiToken}`
                     }
                 });
 
