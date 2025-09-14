c7673f5ea155dd225d9f05f33162cbac1671aae6
Omnia Zayed Taalab Abo ElHassan <omnia.taalab@nis-egypt.com>
2025-09-14 08:08:58 +0000

    File changes

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,16 +5,18 @@ import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getAnalytics, type Analytics } from "firebase/analytics";
 import { getStorage, type FirebaseStorage } from "firebase/storage"; // Import FirebaseStorage
+import { getMessaging, type Messaging } from "firebase/messaging";
 
 // Your project's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBSs_07LU1yI6lNPvq50hs-zP_hrgtPQ84",
   authDomain: "streamlined-hr-assistant.firebaseapp.com",
   projectId: "streamlined-hr-assistant",
-  storageBucket: "streamlined-hr-assistant", // << change this
+  storageBucket: "streamlined-hr-assistant.appspot.com",
   messagingSenderId: "738520001905",
   appId: "1:738520001905:web:b94818595a2713e8251ad0",
-  measurementId: ""
+  measurementId: "",
+  vapidKey: "BE6Xntok3JasNmcwEoWfWAGcFjbUa07XQZ5ecV8Pr1yMmt2Xk9z0PqEHeleNGajjVunl634XtaOu6904GMsw7oA",
 };
 
 // Initialize Firebase
@@ -22,6 +24,7 @@ let app: FirebaseApp;
 let auth: Auth;
 let db: Firestore;
 let storage: FirebaseStorage; // Use FirebaseStorage type
+let messaging: Messaging | null = null;
 let analytics: Analytics | null = null;
 
 if (!getApps().length) {
@@ -30,7 +33,11 @@ if (!getApps().length) {
   db = getFirestore(app);
   storage = getStorage(app);
   if (typeof window !== 'undefined') {
-   console.log("undefined");
+    try {
+        messaging = getMessaging(app);
+    } catch (e) {
+        console.error("Failed to initialize Firebase Messaging", e);
+    }
     try {
         analytics = getAnalytics(app);
      } catch (e) {
@@ -43,13 +50,22 @@ if (!getApps().length) {
   auth = getAuth(app);
   db = getFirestore(app);
   storage = getStorage(app);
-  if (typeof window !== 'undefined' && !analytics) {
-     try {
-        analytics = getAnalytics(app);
-     } catch (e) {
-        console.error("Failed to initialize Firebase Analytics", e);
+  if (typeof window !== 'undefined') {
+     if (!messaging) {
+        try {
+            messaging = getMessaging(app);
+        } catch (e) {
+            console.error("Failed to initialize Firebase Messaging", e);
+        }
+     }
+     if (!analytics) {
+         try {
+            analytics = getAnalytics(app);
+         } catch (e) {
+            console.error("Failed to initialize Firebase Analytics", e);
+         }
      }
   }
 }
 
-export { app, auth, db, storage, analytics }; // Export storage
+export { app, auth, db, storage, messaging, analytics }; // Export storage
