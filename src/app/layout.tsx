0c2d3462fceda5fdf3916326bf55bb627cc41e65
Omnia Zayed Taalab Abo ElHassan <omnia.taalab@nis-egypt.com>
2025-10-06 13:38:42 +0000

    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,6 @@ const fontSpaceGrotesk = Space_Grotesk({
 
 export const metadata: Metadata = {
   title: 'HR Assistant',
-  icons: {
-    icon: "/nis_logo.png",
-  },
   description: 'A modern Human Resource Management System',
 };
 
@@ -31,7 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" dir="ltr" suppressHydrationWarning>
-      <head />
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-body antialiased",
