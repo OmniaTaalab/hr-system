f3ded9d71ef224106816717ea1bb1f0b64ae8820
Omnia Zayed Taalab Abo ElHassan <omnia.taalab@nis-egypt.com>
2025-11-23 09:05:21 +0000

    Try fixing this error: `Console Error: [2025-11-23T09:04:16.179Z] @fireb

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyBSs_07LU1yI6lNPvq50hs-zP_hrgtPQ84",
     authDomain: "streamlined-hr-assistant.firebaseapp.com",
     projectId: "streamlined-hr-assistant",
-    storageBucket: "streamlined-hr-assistant",
+    storageBucket: "streamlined-hr-assistant.appspot.com",
     messagingSenderId: "738520001905",
     appId: "1:738520001905:web:b94818595a2713e8251ad0",
     measurementId: "G-2073PERBQ5"
@@ -26,51 +26,29 @@ let storage: FirebaseStorage; // Use FirebaseStorage type
 let messaging: Messaging | null = null;
 let analytics: Analytics | null = null;
 
-if (typeof window !== 'undefined') {
-  if (!getApps().length) {
-    app = initializeApp(firebaseConfig);
-    auth = getAuth(app);
-    db = getFirestore(app);
-    storage = getStorage(app);
-    try {
-        messaging = getMessaging(app);
-    } catch (e) {
-        console.error("Failed to initialize Firebase Messaging", e);
-    }
-    try {
-        analytics = getAnalytics(app);
-     } catch (e) {
-        console.error("Failed to initialize Firebase Analytics", e);
-     }
-  } else {
-    app = getApps()[0];
-    auth = getAuth(app);
-    db = getFirestore(app);
-    storage = getStorage(app);
-     if (!messaging) {
-        try {
-            messaging = getMessaging(app);
-        } catch (e) {
-            console.error("Failed to initialize Firebase Messaging", e);
-        }
-     }
-     if (!analytics) {
-         try {
-            analytics = getAnalytics(app);
-         } catch (e) {
-            console.error("Failed to initialize Firebase Analytics", e);
-         }
-     }
-  }
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
 } else {
-    if (!getApps().length) {
-        app = initializeApp(firebaseConfig);
-    } else {
-        app = getApps()[0];
-    }
-    auth = getAuth(app);
-    db = getFirestore(app);
-    storage = getStorage(app);
+  app = getApps()[0];
+}
+
+auth = getAuth(app);
+db = getFirestore(app);
+storage = getStorage(app);
+
+if (typeof window !== 'undefined') {
+  try {
+      if ('measurementId' in firebaseConfig) {
+        analytics = getAnalytics(app);
+      }
+  } catch (e) {
+      console.error("Failed to initialize Firebase Analytics", e);
+  }
+  try {
+    messaging = getMessaging(app);
+  } catch (e) {
+      console.error("Failed to initialize Firebase Messaging", e);
+  }
 }
 
 
