4445a3b2cc5a1ab3649af26f9bcb0946dd771d86
Omar Kandil <omar.kandil@nis-egypt.com>
2025-12-22 07:25:16 +0000

    Error occurred prerendering page "/employees". Read more: https://nextjs

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -2,11 +2,14 @@
 "use client"
 import { AppLayout } from "@/components/layout/app-layout";
 import EmployeeManagementContent from "./EmployeeManagementClient";
+import React from 'react';
 
 export default function EmployeeManagementPage() {
   return (
     <AppLayout>
-      <EmployeeManagementContent />
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <EmployeeManagementContent />
+      </React.Suspense>
     </AppLayout>
   );
 }
