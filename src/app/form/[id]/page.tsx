ce91dc4e4636814ef489ca72f073859e0d2cb78e
Omnia Zayed Taalab Abo ElHassan <omnia.taalab@nis-egypt.com>
2026-01-25 08:57:48 +0000

    in online Application Building / floor is not saved

--- src/app/form/[id]/page.tsx
+++ src/app/form/[id]/page.tsx
@@ -240,7 +240,7 @@ function ApplicationDetailContent() {
                   <DetailItem label="Marital Status" value={application.maritalStatus} icon={User}/>
                   <DetailItem label="Parent at NIS" icon={User}><YesNoIcon value={application.isParentAtNIS} /></DetailItem>
                   <DetailItem label="Number of Children" value={application.numberOfChildren} icon={Users}/>
-                  <DetailItem label="Address" value={`${application.apartment}, ${application.street} St, ${application.area}, ${application.city}, ${application.country}`} icon={MapPin} />
+                  <DetailItem label="Address" value={[application.building, application.apartment, application.street, application.area, application.city, application.country].filter(Boolean).join(', ')} icon={MapPin} />
                   <DetailItem label="Contacted By HR" value={application.contactedByHR} icon={Users}/>
 
                 </div>
