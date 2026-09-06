d6a93240070ca4256677b722fec4b74f0612d1af
OmniaTaalab <omnia.taalab@nis-egypt.com>
2026-09-06 22:29:25 +0300

    chore: configure project metadata and build settings
    Update project metadata, enable standalone build mode, and refine development scripts and image host configurations. Clean up redundant configuration files.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -14,6 +14,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
       },
